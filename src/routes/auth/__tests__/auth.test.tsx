8012dbc7f6c37ae11f0aeadb53a8df4d5b025a6d
Simon Bailey <5601214+thebailers@users.noreply.github.com>
2023-02-10 09:52:14 +0000

    Test firebase sign up api errors, mocked response

--- src/routes/auth/__tests__/auth.test.tsx
+++ src/routes/auth/__tests__/auth.test.tsx
@@ -1,8 +1,13 @@
 import { fireEvent, render, screen } from "@testing-library/react";
+import { FirebaseError } from "firebase/app";
 import Auth from "../auth.component";
 import { signUpUserEmailPassword as mockSignUpUserEmailPassword } from "../../../utils/firebase/firebase.utils";
 
 jest.mock("../../../utils/firebase/firebase.utils");
+
+// jest.mock("../../../utils/firebase/firebase.utils", () => {
+//   return jest.fn();
+// });
 
 const setup = () => {
   const utils = render(<Auth />);
@@ -26,8 +31,31 @@ describe("auth component", () => {
     ).toBeInTheDocument();
   });
 
-  it("doesn't call the sign in fn when signin inputs are empty", () => {
-    setup();
+  it("doesn't call the firebase sign in api when signin inputs are empty", () => {
+    const { button } = setup();
+    fireEvent.click(button);
+    expect(mockSignUpUserEmailPassword).toBeCalledTimes(0);
+  });
+
+  it("calls the firebase sign up with email/password api when email & password present", () => {
+    const { inputPassword, inputEmail, button } = setup();
+    fireEvent.change(inputEmail, { target: { value: "billytubbins" } });
+    fireEvent.change(inputPassword, { target: { value: "12345" } });
+    fireEvent.click(button);
     expect(mockSignUpUserEmailPassword).toBeCalledTimes(1);
+  });
+
+  it("displays relevant error message when email address already in use", () => {
+    const { inputPassword, inputEmail, button } = setup();
+    fireEvent.change(inputEmail, { target: { value: "billytubbins" } });
+    fireEvent.change(inputPassword, { target: { value: "12345" } });
+    (mockSignUpUserEmailPassword as jest.Mock).mockImplementation(() => {
+      throw new FirebaseError(
+        "auth/email-already-in-use",
+        "Firebase: Error (auth/email-already-in-use)."
+      );
+    });
+    fireEvent.click(button);
+    expect(screen.getByText(/email address in use/i)).toBeInTheDocument();
   });
 });
