26119fdb4f29ec89d8309755a7ae04a8dab84f6e
Simon Bailey <5601214+thebailers@users.noreply.github.com>
2023-02-24 21:40:29 +0000

    Fix protected route tests

--- src/routes/protected-route/__tests__/protected-route.test.tsx
+++ src/routes/protected-route/__tests__/protected-route.test.tsx
@@ -1,8 +1,7 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import { MemoryRouter, Routes, Route, Navigate } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 import type { DocumentData } from "firebase/firestore";
 
-import Profile from "../../profile/profile.component";
 import ProtectedRoute from "../protected-route.component";
 import Auth from "../../auth/auth.component";
 
@@ -10,28 +9,30 @@ import { UserContext } from "../../../contexts/user.context";
 
 const setCurrentUser = jest.fn();
 
+const setup = (currentUser: DocumentData | null | undefined) =>
+  render(
+    <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+      <MemoryRouter initialEntries={["/profile"]}>
+        <Routes>
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute user={currentUser}>
+                <div data-testid="profile" />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/auth/*" element={<div data-testid="auth" />} />
+        </Routes>
+      </MemoryRouter>
+    </UserContext.Provider>
+  );
+
 describe("protected route", () => {
   it("redirects to auth when user is not logged in", async () => {
-    const currentUser = null;
-    render(
-      <UserContext.Provider value={{ currentUser, setCurrentUser }}>
-        <MemoryRouter initialEntries={["/profile"]}>
-          <Routes>
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute user={currentUser}>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/auth/*" element={<Auth />} />
-          </Routes>
-        </MemoryRouter>
-      </UserContext.Provider>
-    );
-
-    expect(await screen.findByText(/sign in/i)).not.toBeInTheDocument();
+    setup(null);
+    expect(screen.queryByTestId("profile")).not.toBeInTheDocument();
+    expect(screen.getByTestId("auth")).toBeInTheDocument();
   });
 
   it("displays the user display name when logged in", () => {
@@ -39,20 +40,8 @@ describe("protected route", () => {
       email: "billy@tubbins.com",
       displayName: "Billy Tubbins",
     };
-    render(
-      <UserContext.Provider value={{ currentUser, setCurrentUser }}>
-        <MemoryRouter>
-          <ProtectedRoute user={currentUser}>
-            <Profile />
-          </ProtectedRoute>
-        </MemoryRouter>
-      </UserContext.Provider>
-    );
-    expect(screen.getByText(/billy tubbins/i)).toBeInTheDocument();
+    setup(currentUser);
+    expect(screen.getByTestId("profile")).toBeInTheDocument();
+    expect(screen.queryByTestId("auth")).not.toBeInTheDocument();
   });
-
-  // it("redirects to authdsfsdhen user is not logged in", () => {
-  //   setup();
-  //   expect(screen.getByText(/billy tubbins/i)).toBeInTheDocument();
-  // });
 });
