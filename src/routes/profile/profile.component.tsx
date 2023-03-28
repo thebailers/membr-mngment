1774311e92ae5cee823d7f1187f80cb7e97eac06
Simon Bailey <5601214+thebailers@users.noreply.github.com>
2023-03-28 21:13:40 +0100

    Profile schema wip

--- src/routes/profile/profile.component.tsx
+++ src/routes/profile/profile.component.tsx
@@ -6,6 +6,29 @@ const Profile = () => {
   return (
     <div>
       <h1>{currentUser!.displayName}</h1>
+      <dl>
+        <dt>member_since</dt>
+        <dd>Date</dd>
+        <dt>emergency_contact</dt>
+        <dd>number</dd>
+        <dt>training_record</dt>
+        <dd>[training-record]</dd>
+      </dl>
+
+      <h2>Training record</h2>
+      <dl>
+        <dt>belt_color</dt>
+        <dd>[belt-colors]</dd>
+      </dl>
+
+      <h2>Belt colours</h2>
+      <ul>
+        <li>white</li>
+        <li>blue</li>
+        <li>purple</li>
+        <li>brown</li>
+        <li>black</li>
+      </ul>
     </div>
   );
 };
